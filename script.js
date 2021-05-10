8e4f64cd5de217b603941014532746c0bd2da7ab
sanchezraven <sanchez_raven@mail.ru>
2021-05-10 13:15:07 +0500

    refactor

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-let sect = document.querySelectorAll("li");
+window.onload = () => {
+    let sect = document.querySelectorAll("li");
+    let content = document.querySelector(".content");
 
-sect[0].addEventListener("click", f1);
-function f1(){
-    document.querySelector('.content').innerHTML = document.querySelector('.About').innerHTML;
+    content.innerHTML = document.querySelector('.About').innerHTML;
+
+    for(let i = 0; i < sect.length; i++) {
+        sect[i].onclick = (e) => {
+            let target = e.target;
+            if (target.id == 0) {
+                content.innerHTML = document.querySelector('.About').innerHTML;
+            } else if (target.id == 1) {
+                content.innerHTML = document.querySelector('.Exp').innerHTML;
+            } else if (target.id == 2) {
+                content.innerHTML = document.querySelector('.Job').innerHTML;
+            } else if (target.id == 3) {
+                content.innerHTML = document.querySelector('.Edu').innerHTML;
+            }
+        }
+    }
 }
-
-sect[1].addEventListener("click", f2);
-function f2(){
-    document.querySelector('.content').innerHTML = document.querySelector('.Exp').innerHTML;
-}
-
-sect[2].addEventListener("click", f3);
-function f3(){
-    document.querySelector('.content').innerHTML = document.querySelector('.Job').innerHTML;
-}
-
-sect[3].addEventListener("click", f4);
-function f4(){
-    document.querySelector('.content').innerHTML = document.querySelector('.Edu').innerHTML;
-}
-
-window.onload = f1;
